2a2fd87c1174004e98acdc2b133bf56202980aee
jafarimahdi <mahdijafarii.mj@gmail.com>
2020-11-30 06:30:03 +0100

    delete item

--- php&mySql/www/project/ninja/details.php
+++ php&mySql/www/project/ninja/details.php
@@ -3,12 +3,27 @@
 // check the get request id parameter
 include 'config/db_connect.php';
 
-// -1
+if(isset($_POST['delete'])){
+    $id_to_delete = mysqli_real_escape_string($conn, $_POST['id_to_delete']);
+
+    // use the double quotation is important " "
+    $sql = "DELETE FROM pizzas WHERE id = $id_to_delete";
+
+    if(mysqli_query($conn, $sql)){
+        // success
+        header('Location:index.php');
+    }{
+        //failure
+        echo 'query error:' . mysqli_error($conn);
+    }
+}
+
+    // -1
 if ( isset( $_GET['id'] ) ) {
-    // -2 check the dataBase hae this id
+    // -2 check the dataBase has this id
     $id = mysqli_real_escape_string( $conn, $_GET['id'] );
 
-    //-3 make sql select
+    //-3 select the data from dataBase
     $sql = "SELECT * FROM pizzas WHERE id = $id";
 
     //-4 get query result
@@ -20,23 +35,28 @@ if ( isset( $_GET['id'] ) ) {
     // -6 make it free
     mysqli_free_result( $result );
     mysqli_close( $conn );
-
-    // print_r( $pizza );
-
 }
 
 ?>
 <!DOCTYPE html>
 <html lang="en">
-
     <?php include 'template/header.php';?>
+
     <div class="container center">
         <?php if ( $pizza ): ?>
+
             <h4><?php echo htmlspecialchars( $pizza['title'] ); ?></h4>
             <p> Created By: <?php echo htmlspecialchars( $pizza['email'] ); ?></p>
+            <p> With ID: <?php echo htmlspecialchars( $pizza['id'] ); ?></p>
             <p><?php echo date($pizza['created_at']); ?></p>
             <h5>Ingredients: </h5>
             <p><?php echo htmlspecialchars( $pizza['ingredients'] ); ?></p>
+            
+            <!-- Delete Form  -->
+            <form action="details.php" method="POST">
+                <input type="hidden" name ="id_to_delete" value ="<?php echo $pizza['id']?> ;">
+                <input type="submit" name="delete" value ="Delete" class="btn brand z-depth-0">
+            </form>
         <?php else: ?>
             <h5>No such pizza exists </h5>
 
